ef6ce18a92d055de10b51ff0aa92415a2c10efdc
Max O'Donnell <screwyprof@gmail.com>
2019-04-01 00:52:08 +0300

    Console printer cannot be created if required parameters are not passed.

--- examples/bank/internal/ui/console_printer.go
+++ examples/bank/internal/ui/console_printer.go
@@ -17,10 +17,21 @@ func NewConsolePrinter(w io.Writer, accountReporter report.GetAccountDetails) *C
 	if w == nil {
 		panic("writer is required")
 	}
+
+	if accountReporter == nil {
+		panic("accountReporter is required")
+	}
 	return &ConsolePrinter{w: w, accountReporter: accountReporter}
 }
 
 // PrintAccountStatement prints account statement to console.
+//
+// Sample output:
+// Account #ACC777:
+// # |  Amount | Balance
+// 1 | 1000.00 | 1000.00
+// 2 | -100.00 | 900.00
+// 3 |  500.00 | 1400.00
 func (p *ConsolePrinter) PrintAccountStatement() {
 	//accountDetailedReport := accountReporter.AccountDetailsFor(ID)
 	panic("Implement me")
--- examples/bank/internal/ui/cosole_printer_test.go
+++ examples/bank/internal/ui/cosole_printer_test.go
@@ -1,6 +1,7 @@
 package ui_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/screwyprof/cqrs/pkg/assert"
@@ -12,6 +13,13 @@ func TestNewConsolePrinter(t *testing.T) {
 	t.Run("ItPanicsIfWriterIsNotGiven", func(t *testing.T) {
 		factory := func() {
 			ui.NewConsolePrinter(nil, nil)
+		}
+		assert.Panic(t, factory)
+	})
+
+	t.Run("ItPanicsIfAccountReporterIsNotGiven", func(t *testing.T) {
+		factory := func() {
+			ui.NewConsolePrinter(&bytes.Buffer{}, nil)
 		}
 		assert.Panic(t, factory)
 	})
