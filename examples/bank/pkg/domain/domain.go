0bd97d08a68b79403754c6980626f76660da9af8
Max O'Donnell <screwyprof@gmail.com>
2019-04-01 02:37:19 +0300

    Linter warnings fixed

--- examples/bank/pkg/domain/domain.go
+++ examples/bank/pkg/domain/domain.go
@@ -6,9 +6,9 @@ import "fmt"
 type Identifier = fmt.Stringer
 
 //
-// DomainEvent represents something that took place in the domain.
+// Event represents something that took place in the domain.
 //
 // Events are always named with a past-participle verb, such as OrderConfirmed.
-type DomainEvent interface {
+type Event interface {
 	EventType() string
 }
